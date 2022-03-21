f9c788fae4bb788873dc569651c28e7c4f8aadc3
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-03-22 02:24:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,37 +16,27 @@ app = Flask(__name__)
 
 @app.route("/get-movie")
 def get_movie():
-    return jsonify({
-        "data": all_movies[0],
-        "status": "success"
-    })
+   
 
-@app.route("/liked-movie", methods=["POST"])
+@app.route("/liked-movie",   )
 def liked_movie():
     movie = all_movies[0]
     all_movies = all_movies[1:]
-    liked_movies.append(movie)
-    return jsonify({
-        "status": "success"
-    }), 201
+    
+   
 
-@app.route("/unliked-movie", methods=["POST"])
+@app.route("/unliked-movie",)
 def unliked_movie():
     movie = all_movies[0]
     all_movies = all_movies[1:]
-    not_liked_movies.append(movie)
-    return jsonify({
-        "status": "success"
-    }), 201
+    
+   
 
-@app.route("/did-not-watch", methods=["POST"])
+@app.route("/did-not-watch", )
 def did_not_watch():
     movie = all_movies[0]
     all_movies = all_movies[1:]
-    did_not_watch.append(movie)
-    return jsonify({
-        "status": "success"
-    }), 201
+    
 
 if __name__ == "__main__":
   app.run()
